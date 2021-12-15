fe2a1d32ce9743313a2df0f74e2b23a6cdd419df
vasiljeu95 <vasilyeu95@gmail.com>
2021-12-16 01:24:02 +0300

    classDriverCommit

--- src/com/github/vasiljeu95/tmshw5/task2/Task2.java
+++ src/com/github/vasiljeu95/tmshw5/task2/Task2.java
@@ -3,4 +3,49 @@ package com.github.vasiljeu95.tmshw5.task2;
 public class Task2 {
 }
 
+class Human {
+    String nameHuman;
+    int ageHuman;
+    int massHuman;
+    int higthHuman;
+    int iqHuman;
 
+    void runningHuman() {
+        System.out.println("RUN!");
+    }
+
+    void swimmingHuman() {
+        System.out.println("SWIM!");
+    }
+
+    void bicycleHuman() {
+        System.out.println("Keep pedaling!");
+    }
+}
+
+class Driver extends Human {
+    String drivingExperience;
+    String driversCattegory;
+    boolean medicalCertificate;
+
+    void hasMedicalCertificate () {
+        if (medicalCertificate == true) {
+            System.out.println("The driver " + nameHuman + " has a medical Certificate");
+        } else {
+            System.out.println("The driver " + nameHuman + " has NOT a medical Certificate");
+        }
+    }
+}
+
+
+class DriversLicense extends Human {
+    boolean driversLicense;
+
+    void hasDriversLicense () {
+        if (driversLicense == true) {
+            System.out.println("The driver " + nameHuman + " has a license");
+        } else {
+            System.out.println("The driver " + nameHuman + " has NOT a license");
+        }
+    }
+}
