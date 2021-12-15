982054db73d8d170f488472df4c110e4c4e6c257
vasiljeu95 <vasilyeu95@gmail.com>
2021-12-16 00:53:33 +0300

    Initial commit

---
+++ src/com/github/vasiljeu95/tmshw5/task1/Task1.java
@@ -0,0 +1,18 @@
+package com.github.vasiljeu95.tmshw5.task1;
+
+public class Task1 {
+}
+
+class Dog {
+    String breedDog;
+    String colorDog;
+    int sizeDog;
+
+    void barksDog() {
+        System.out.println("barks! barks! barks!");
+    }
+
+    void sleepDog() {
+        System.out.println("Sleep! Sleep! Sleep!");
+    }
+}
---
+++ src/com/github/vasiljeu95/tmshw5/task2/Task2.java
@@ -0,0 +1,6 @@
+package com.github.vasiljeu95.tmshw5.task2;
+
+public class Task2 {
+}
+
+
